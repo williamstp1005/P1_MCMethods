26961497b5642c26427e9bb101218ff4a2857a94
William St. Peter <108362005+williamstp1005@users.noreply.github.com>
2024-01-17 15:20:14 -0800

    Add files via upload

--- Q2.cpp
+++ Q2.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main() {
 	srand(time(0));
-	int N = 50;
+	int N = 10;
 	int steps = 320;
 	int** row = new int* [N];
 	for (int i = 0; i < N; i++) {
@@ -42,61 +42,69 @@ int main() {
 		}
 		cout << endl;
 	}
-	cout << numneg << "    " << numpos << endl;
-	cout << "--------------------------------------------------------------------------------------------------------------------" << endl;
+	//cout << numneg << "    " << numpos << endl;
+	int stepLimit = 10;
 
 	for (int i = 0; i < steps; i++) {
-		x = rand() % N;
-		y = rand() % N;
-		//cout << "\n\n\n\n\nx: " << x << "    y: " << y << "\n\n\n\n";
-		int summation = 0;
+		cout << "\n\n--------------------------------------------------------------------------------------------------------------------\n" << "Steps taken: " << stepLimit << "\n\n" << endl;
+		while (i < stepLimit) 
+		{
+			x = rand() % N;
+			y = rand() % N;
+			//cout << "\n\n\n\n\nx: " << x << "    y: " << y << "\n\n\n\n";
+			int summation = 0;
 
-		if (x == 0 && y == 0) {
-			summation = row[x][y + 1] + row[x + 1][y];
-		}
-		else if (x == 0 && y == N - 1) {
-			summation = row[x + 1][y] + row[x][y - 1];
-		}
-		else if (x == N - 1 && y == 0) {
-			summation = row[x - 1][y] + row[x][y + 1];
-		}
-		else if (x == N - 1 && y == N - 1) {
-			summation = row[x - 1][y] + row[x][y - 1];
-		}
-		else if (x == 0) {
-			summation = row[x][y + 1] + row[x][y - 1] + row[x + 1][y];
-		}
-		else if (x == N - 1) {
-			summation = row[x][y + 1] + row[x][y - 1] + row[x - 1][y];
-		}
-		else if (y == 0) {
-			summation = row[x][y + 1] + row[x - 1][y] + row[x + 1][y];
-		}
-		else if (y == N - 1) {
-			summation = row[x - 1][y] + row[x][y - 1] + row[x + 1][y];
-		}
-		else {
-			summation = row[x - 1][y] + row[x][y - 1] + row[x + 1][y] + row[x][y + 1];
-		}
-
-		deltE = J * row[x][y] * summation;
-		double prob = exp(deltE / (k * T));
-		int change = ((((float)rand() / RAND_MAX) <= prob) * -2 + 1);
-		row[x][y] *= change;
-		//cout << change << endl;
-	}
-
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++) {
-			if (row[i][j] == -1) {
-				cout << ' ';
+			if (x == 0 && y == 0) {
+				summation = row[x][y + 1] + row[x + 1][y];
+			}
+			else if (x == 0 && y == N - 1) {
+				summation = row[x + 1][y] + row[x][y - 1];
+			}
+			else if (x == N - 1 && y == 0) {
+				summation = row[x - 1][y] + row[x][y + 1];
+			}
+			else if (x == N - 1 && y == N - 1) {
+				summation = row[x - 1][y] + row[x][y - 1];
+			}
+			else if (x == 0) {
+				summation = row[x][y + 1] + row[x][y - 1] + row[x + 1][y];
+			}
+			else if (x == N - 1) {
+				summation = row[x][y + 1] + row[x][y - 1] + row[x - 1][y];
+			}
+			else if (y == 0) {
+				summation = row[x][y + 1] + row[x - 1][y] + row[x + 1][y];
+			}
+			else if (y == N - 1) {
+				summation = row[x - 1][y] + row[x][y - 1] + row[x + 1][y];
 			}
 			else {
-				cout << '#';
+				summation = row[x - 1][y] + row[x][y - 1] + row[x + 1][y] + row[x][y + 1];
 			}
+
+			deltE = J * row[x][y] * summation;
+			double prob = exp(deltE / (k * T));
+			int change = ((((float)rand() / RAND_MAX) <= prob) * -2 + 1);
+			row[x][y] *= change;
+			i++;
+			//cout << change << endl;
 		}
-		cout << endl;
+		//cout << "Steps taken: " << stepLimit << endl;
+		stepLimit *= 2;
+		for (int j = 0; j < N; j++) {
+			for (int k = 0; k < N; k++) {
+				if (row[j][k] == -1) {
+					cout << ' ';
+				}
+				else {
+					cout << '#';
+				}
+			}
+			cout << endl;
+		}
 	}
+
+	
 
 
 	return 0;
